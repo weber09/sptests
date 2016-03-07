db9a393d2ea42f0903f78d01e20af8f0c4450fe1
Gabriel Weber <gabrieldsweber@gmail.com>
2016-03-06 23:45:37 -0300

    *added more methods

--- src/algol/CLFile.java
+++ src/algol/CLFile.java
@@ -7,6 +7,8 @@ package algol;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import static algol.CLConstants.*;
+
 public class CLFile {
 
     public long magic; // 0xCAFEBABE
@@ -76,6 +78,205 @@ public class CLFile {
                 attributeInfo.write(out);
             }
         }
+    }
+
+    public static String classAccessFlagsToString(int accessFlags) {
+        StringBuffer b = new StringBuffer();
+        if ((accessFlags & ACC_PUBLIC) != 0) {
+            b.append("public ");
+        }
+        if ((accessFlags & ACC_FINAL) != 0) {
+            b.append("final ");
+        }
+        if ((accessFlags & ACC_SUPER) != 0) {
+            b.append("super ");
+        }
+        if ((accessFlags & ACC_INTERFACE) != 0) {
+            b.append("interface ");
+        }
+        if ((accessFlags & ACC_ABSTRACT) != 0) {
+            b.append("abstract ");
+        }
+        if ((accessFlags & ACC_SYNTHETIC) != 0) {
+            b.append("synthetic ");
+        }
+        if ((accessFlags & ACC_ANNOTATION) != 0) {
+            b.append("annotation ");
+        }
+        if ((accessFlags & ACC_ENUM) != 0) {
+            b.append("enum ");
+        }
+        return b.toString().trim();
+    }
+
+    public static String innerClassAccessFlagsToString(int accessFlags) {
+        StringBuffer b = new StringBuffer();
+        if ((accessFlags & ACC_PUBLIC) != 0) {
+            b.append("public ");
+        }
+        if ((accessFlags & ACC_PRIVATE) != 0) {
+            b.append("private ");
+        }
+        if ((accessFlags & ACC_PROTECTED) != 0) {
+            b.append("protected ");
+        }
+        if ((accessFlags & ACC_STATIC) != 0) {
+            b.append("static ");
+        }
+        if ((accessFlags & ACC_FINAL) != 0) {
+            b.append("final ");
+        }
+        if ((accessFlags & ACC_INTERFACE) != 0) {
+            b.append("interface ");
+        }
+        if ((accessFlags & ACC_ABSTRACT) != 0) {
+            b.append("abstract ");
+        }
+        if ((accessFlags & ACC_SYNTHETIC) != 0) {
+            b.append("synthetic ");
+        }
+        if ((accessFlags & ACC_ANNOTATION) != 0) {
+            b.append("annotation ");
+        }
+        if ((accessFlags & ACC_ENUM) != 0) {
+            b.append("enum ");
+        }
+        return b.toString().trim();
+    }
+
+    public static String fieldAccessFlagsToString(int accessFlags) {
+        StringBuffer b = new StringBuffer();
+        if ((accessFlags & ACC_PUBLIC) != 0) {
+            b.append("public ");
+        }
+        if ((accessFlags & ACC_PRIVATE) != 0) {
+            b.append("private ");
+        }
+        if ((accessFlags & ACC_PROTECTED) != 0) {
+            b.append("protected ");
+        }
+        if ((accessFlags & ACC_STATIC) != 0) {
+            b.append("static ");
+        }
+        if ((accessFlags & ACC_FINAL) != 0) {
+            b.append("final ");
+        }
+        if ((accessFlags & ACC_VOLATILE) != 0) {
+            b.append("volatile ");
+        }
+        if ((accessFlags & ACC_TRANSIENT) != 0) {
+            b.append("transient ");
+        }
+        if ((accessFlags & ACC_NATIVE) != 0) {
+            b.append("native ");
+        }
+        if ((accessFlags & ACC_SYNTHETIC) != 0) {
+            b.append("synthetic ");
+        }
+        if ((accessFlags & ACC_ENUM) != 0) {
+            b.append("enum ");
+        }
+        return b.toString().trim();
+    }
+
+    public static String methodAccessFlagsToString(int accessFlags) {
+        StringBuffer b = new StringBuffer();
+        if ((accessFlags & ACC_PUBLIC) != 0) {
+            b.append("public ");
+        }
+        if ((accessFlags & ACC_PRIVATE) != 0) {
+            b.append("private ");
+        }
+        if ((accessFlags & ACC_PROTECTED) != 0) {
+            b.append("protected ");
+        }
+        if ((accessFlags & ACC_STATIC) != 0) {
+            b.append("static ");
+        }
+        if ((accessFlags & ACC_FINAL) != 0) {
+            b.append("final ");
+        }
+        if ((accessFlags & ACC_SYNCHRONIZED) != 0) {
+            b.append("synchronized ");
+        }
+        if ((accessFlags & ACC_BRIDGE) != 0) {
+            b.append("bridge ");
+        }
+        if ((accessFlags & ACC_VARARGS) != 0) {
+            b.append("varargs ");
+        }
+        if ((accessFlags & ACC_NATIVE) != 0) {
+            b.append("native ");
+        }
+        if ((accessFlags & ACC_ABSTRACT) != 0) {
+            b.append("abstract ");
+        }
+        if ((accessFlags & ACC_STRICT) != 0) {
+            b.append("strict ");
+        }
+        if ((accessFlags & ACC_SYNTHETIC) != 0) {
+            b.append("synthetic ");
+        }
+        return b.toString().trim();
+    }
+
+    public static int accessFlagToInt(String accessFlag) {
+        int flag = 0;
+        if (accessFlag.equals("public")) {
+            flag = ACC_PUBLIC;
+        }
+        if (accessFlag.equals("private")) {
+            flag = ACC_PRIVATE;
+        }
+        if (accessFlag.equals("protected")) {
+            flag = ACC_PROTECTED;
+        }
+        if (accessFlag.equals("static")) {
+            flag = ACC_STATIC;
+        }
+        if (accessFlag.equals("final")) {
+            flag = ACC_FINAL;
+        }
+        if (accessFlag.equals("super")) {
+            flag = ACC_SUPER;
+        }
+        if (accessFlag.equals("synchronized")) {
+            flag = ACC_SYNCHRONIZED;
+        }
+        if (accessFlag.equals("volatile")) {
+            flag = ACC_VOLATILE;
+        }
+        if (accessFlag.equals("bridge")) {
+            flag = ACC_BRIDGE;
+        }
+        if (accessFlag.equals("transient")) {
+            flag = ACC_TRANSIENT;
+        }
+        if (accessFlag.equals("varargs")) {
+            flag = ACC_VARARGS;
+        }
+        if (accessFlag.equals("native")) {
+            flag = ACC_NATIVE;
+        }
+        if (accessFlag.equals("interface")) {
+            flag = ACC_INTERFACE;
+        }
+        if (accessFlag.equals("abstract")) {
+            flag = ACC_ABSTRACT;
+        }
+        if (accessFlag.equals("strict")) {
+            flag = ACC_STRICT;
+        }
+        if (accessFlag.equals("synthetic")) {
+            flag = ACC_SYNTHETIC;
+        }
+        if (accessFlag.equals("annotation")) {
+            flag = ACC_ANNOTATION;
+        }
+        if (accessFlag.equals("enum")) {
+            flag = ACC_ENUM;
+        }
+        return flag;
     }
 
 }
