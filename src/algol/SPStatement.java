6ce44b54ebe326dae5af6892a102ac275d06e634
Gabriel Weber <gabrieldsweber@gmail.com>
2016-03-07 21:50:29 -0300

    * first commit

---
+++ src/algol/SPStatement.java
@@ -0,0 +1,14 @@
+package algol;
+
+/**
+ * Created by Gabriel on 07/03/2016.
+ */
+
+abstract class SPStatement extends SPAST {
+
+    protected SPStatement(int line) {
+        super(line);
+    }
+
+}
+
