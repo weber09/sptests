68ffc5f9d6ad0e08582829dd8b857e39e22740eb
Gabriel Weber <gabrieldsweber@gmail.com>
2016-03-23 21:08:48 -0300

    * refactoring

--- src/algol/SPBinaryExpression.java
+++ src/algol/SPBinaryExpression.java
@@ -7,16 +7,14 @@ import static algol.CLConstants.*;
 
 abstract class SPBinaryExpression extends SPExpression {
 
-    protected String operator;
-
     protected SPExpression lhs;
 
     protected SPExpression rhs;
 
-    protected SPBinaryExpression(int line, String operator, SPExpression lhs,
-                                SPExpression rhs) {
+    protected int opcode;
+
+    protected SPBinaryExpression(int line, SPExpression lhs, SPExpression rhs) {
         super(line);
-        this.operator = operator;
         this.lhs = lhs;
         this.rhs = rhs;
     }
@@ -25,65 +23,105 @@ abstract class SPBinaryExpression extends SPExpression {
 class SPPlusOp extends SPBinaryExpression {
 
     public SPPlusOp(int line, SPExpression lhs, SPExpression rhs) {
-        super(line, "+", lhs, rhs);
+        super(line, lhs, rhs);
     }
 
     public SPExpression analyze(Context context) {
-        lhs = (SPExpression) lhs.analyze(context);
-        rhs = (SPExpression) rhs.analyze(context);
+        lhs = lhs.analyze(context);
+        rhs = rhs.analyze(context);
         if (lhs.type() == Type.STRING || rhs.type() == Type.STRING) {
             return (new SPStringConcatenationOp(line, lhs, rhs))
                     .analyze(context);
         } else if (lhs.type() == Type.INT && rhs.type() == Type.INT) {
             type = Type.INT;
+            opcode = IADD;
+        } else if(lhs.type() == Type.DECIMAL || rhs.type() == Type.DECIMAL) {
+            type = Type.DECIMAL;
+            opcode = DADD;
+        } else if(lhs.type() == Type.LONG && rhs.type() == Type.LONG){
+          type = Type.LONG;
+            opcode = LADD;
         } else {
             type = Type.ANY;
-            SPAST.compilationUnit.reportSemanticError(line(),
-                    "Invalid operand types for +");
+            SPAST.compilationUnit.reportSemanticError(line(), "Invalid operand types for +");
         }
         return this;
     }
 
+    private void convert(CLEmitter output, SPExpression exp){
+        if(type != Type.DECIMAL)
+            return;
 
-    public void codegen(CLEmitter output) {
-        if (type == Type.INT) {
-            lhs.codegen(output);
-            rhs.codegen(output);
-            output.addNoArgInstruction(IADD);
-        }
+        int convertOpCode = I2D;
+        if(exp.type() == Type.LONG)
+            convertOpCode = L2D;
+
+        output.addNoArgInstruction(convertOpCode);
     }
 
-}
 
+    public void codegen(CLEmitter output) {
+
+        lhs.codegen(output);
+
+        convert(output, lhs);
+
+        rhs.codegen(output);
+
+        convert(output, rhs);
+
+        output.addNoArgInstruction(opcode);
+    }
+}
 
 class SPSubtractOp extends SPBinaryExpression {
 
     public SPSubtractOp(int line, SPExpression lhs, SPExpression rhs) {
-        super(line, "-", lhs, rhs);
+        super(line, lhs, rhs);
     }
 
     public SPExpression analyze(Context context) {
-        lhs = (SPExpression) lhs.analyze(context);
-        rhs = (SPExpression) rhs.analyze(context);
-        lhs.type().mustMatchExpected(line(), Type.INT);
-        rhs.type().mustMatchExpected(line(), Type.INT);
-        type = Type.INT;
+        lhs = lhs.analyze(context);
+        rhs = rhs.analyze(context);
+        if (lhs.type() == Type.INT && rhs.type() == Type.INT) {
+            type = Type.INT;
+            opcode = ISUB;
+        } else if(lhs.type() == Type.DECIMAL || rhs.type() == Type.DECIMAL) {
+            type = Type.DECIMAL;
+            opcode = DSUB;
+        } else if(lhs.type() == Type.LONG && rhs.type() == Type.LONG){
+            type = Type.LONG;
+            opcode = LSUB;
+        } else {
+            type = Type.ANY;
+            SPAST.compilationUnit.reportSemanticError(line(), "Invalid operand types for -");
+        }
         return this;
     }
 
-    /**
-     * Generating code for the - operation involves generating code for the two
-     * operands, and then the subtraction instruction.
-     *
-     * @param output
-     *            the code emitter (basically an abstraction for producing the
-     *            .class file).
-     */
+    private void convert(CLEmitter output, SPExpression exp){
+        if(type != Type.DECIMAL)
+            return;
+
+        int convertOpCode = I2D;
+        if(exp.type() == Type.LONG)
+            convertOpCode = L2D;
+
+        output.addNoArgInstruction(convertOpCode);
+    }
+
 
     public void codegen(CLEmitter output) {
+
         lhs.codegen(output);
+
+        convert(output, lhs);
+
         rhs.codegen(output);
-        output.addNoArgInstruction(ISUB);
+
+        convert(output, rhs);
+
+        output.addNoArgInstruction(opcode);
     }
 
 }
@@ -92,22 +130,51 @@ class SPMultiplyOp extends SPBinaryExpression {
 
 
     public SPMultiplyOp(int line, SPExpression lhs, SPExpression rhs) {
-        super(line, "*", lhs, rhs);
+        super(line, lhs, rhs);
     }
 
     public SPExpression analyze(Context context) {
-        lhs = (SPExpression) lhs.analyze(context);
-        rhs = (SPExpression) rhs.analyze(context);
-        lhs.type().mustMatchExpected(line(), Type.INT);
-        rhs.type().mustMatchExpected(line(), Type.INT);
-        type = Type.INT;
+        lhs = lhs.analyze(context);
+        rhs = rhs.analyze(context);
+        if (lhs.type() == Type.INT && rhs.type() == Type.INT) {
+            type = Type.INT;
+            opcode = IMUL;
+        } else if(lhs.type() == Type.DECIMAL || rhs.type() == Type.DECIMAL) {
+            type = Type.DECIMAL;
+            opcode = DMUL;
+        } else if(lhs.type() == Type.LONG && rhs.type() == Type.LONG){
+            type = Type.LONG;
+            opcode = LMUL;
+        } else {
+            type = Type.ANY;
+            SPAST.compilationUnit.reportSemanticError(line(), "Invalid operand types for *");
+        }
         return this;
     }
 
+    private void convert(CLEmitter output, SPExpression exp){
+        if(type != Type.DECIMAL)
+            return;
+
+        int convertOpCode = I2D;
+        if(exp.type() == Type.LONG)
+            convertOpCode = L2D;
+
+        output.addNoArgInstruction(convertOpCode);
+    }
+
+
     public void codegen(CLEmitter output) {
+
         lhs.codegen(output);
+
+        convert(output, lhs);
+
         rhs.codegen(output);
-        output.addNoArgInstruction(IMUL);
+
+        convert(output, rhs);
+
+        output.addNoArgInstruction(opcode);
     }
 }
 
@@ -115,22 +182,51 @@ class SPDivisionOp extends SPBinaryExpression {
 
 
     public SPDivisionOp(int line, SPExpression lhs, SPExpression rhs) {
-        super(line, "*", lhs, rhs);
+        super(line, lhs, rhs);
     }
 
     public SPExpression analyze(Context context) {
-        lhs = (SPExpression) lhs.analyze(context);
-        rhs = (SPExpression) rhs.analyze(context);
-        lhs.type().mustMatchExpected(line(), Type.INT);
-        rhs.type().mustMatchExpected(line(), Type.INT);
-        type = Type.INT;
+        lhs = lhs.analyze(context);
+        rhs = rhs.analyze(context);
+        if (lhs.type() == Type.INT && rhs.type() == Type.INT) {
+            type = Type.INT;
+            opcode = IDIV;
+        } else if(lhs.type() == Type.DECIMAL || rhs.type() == Type.DECIMAL) {
+            type = Type.DECIMAL;
+            opcode = DDIV;
+        } else if(lhs.type() == Type.LONG && rhs.type() == Type.LONG){
+            type = Type.LONG;
+            opcode = LDIV;
+        } else {
+            type = Type.ANY;
+            SPAST.compilationUnit.reportSemanticError(line(), "Invalid operand types for /");
+        }
         return this;
     }
 
+    private void convert(CLEmitter output, SPExpression exp){
+        if(type != Type.DECIMAL)
+            return;
+
+        int convertOpCode = I2D;
+        if(exp.type() == Type.LONG)
+            convertOpCode = L2D;
+
+        output.addNoArgInstruction(convertOpCode);
+    }
+
+
     public void codegen(CLEmitter output) {
+
         lhs.codegen(output);
+
+        convert(output, lhs);
+
         rhs.codegen(output);
-        output.addNoArgInstruction(IDIV);
+
+        convert(output, rhs);
+
+        output.addNoArgInstruction(opcode);
     }
 }
 
